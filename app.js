003636192e58c154560ed3ca74a1ef5292756a48
Isabela <ilujanj@unal.edu.co>
2020-03-10 18:34:35 -0500

    Avances 1
    Ya es un vómito de colores y tiene dos funciones que llaman a cada IP

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ function validarInput() {
     document.getElementById("btn-1ip2").disabled = !document.getElementById("IP2").value.length || (document.getElementById("IP1").value.length>0);
     document.getElementById("btn-2ip").disabled = !document.getElementById("IP1").value.length || !document.getElementById("IP2").value.length
   }
+
+  function unaIP1(){
+    var IP1 = document.getElementById("IP1").value;
+  }
+
+  function unaIP2(){
+    var IP2 = document.getElementById("IP2").value;
+  }

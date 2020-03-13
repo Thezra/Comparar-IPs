a7f94cdc9541566f52f025c9210c02a6fd93bbd4
Isabela <ilujanj@unal.edu.co>
2020-03-12 23:09:49 -0500

    Avances 2
    Cuatro campos numéricos para cada IP y lo que conlleva el cambio

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
-function validarInput() {
-    document.getElementById("btn-1ip1").disabled = !document.getElementById("IP1").value.length || (document.getElementById("IP2").value.length>0);
+/*function validarInput() {
+    //Condicion de min y max
+    document.getElementById("btn-1ip1").disabled = (!document.getElementById("IP1_1").value.length &&
+    	!document.getElementById("IP1_2").value.length && !document.getElementById("IP1_3").value.length
+    	&& !document.getElementById("IP1_4").value.length) || 
+    (document.getElementById("IP2_1").value.length>0 || document.getElementById("IP2_2").value.length>0 ||
+    	document.getElementById("IP2_3").value.length>0 || document.getElementById("IP2_4").value.length>0);
+    
+
     document.getElementById("btn-1ip2").disabled = !document.getElementById("IP2").value.length || (document.getElementById("IP1").value.length>0);
     document.getElementById("btn-2ip").disabled = !document.getElementById("IP1").value.length || !document.getElementById("IP2").value.length
-  }
+  }*/
 
-  function unaIP1(){
-    var IP1 = document.getElementById("IP1").value;
-  }
+var ElementosIP1 = []
+var ElementosIP2 = []
 
-  function unaIP2(){
-    var IP2 = document.getElementById("IP2").value;
-  }
+// --- Verificación botón de una IP1  y lógica ---
+ function unaIP1(){
+ 	for (var i=1; i<=4; i++){
+ 		var id = "IP1_"+i
+ 		var num = parseInt(document.getElementById(id).value)
+ 		if (!isNaN(num) && num >= 0 && num <= 255){
+ 			ElementosIP1.push(num)
+ 		}
+ 	}
+ 	if (Lista.length < 4){
+ 		alert ("El campo de la primera IP debe estar lleno y cada elemento de la IP debe ser un numero entero entre 0 y 255")
+ 		ElementosIP1 = []
+ 	}else{
+ 		return ("OK")
+ 		primeraIP(ElementosIP1)
+ 		ElementosIP1 = []
+ 	}
+ }
+
+// --- Verificación botón de una IP1  y lógica ---
+ function unaIP2(){
+ 	for (var j=1; j<=4; j++){
+ 		var id = "IP2_"+j
+ 		var num = parseInt(document.getElementById(id).value)
+ 		if (!isNaN(num) && num >= 0 && num <= 255){
+ 			ElementosIP2.push(num)
+ 		}
+ 	}
+ 	if (Lista.length < 4){
+ 		alert ("El campo de la segundaa IP debe estar lleno y cada elemento de la IP debe ser un numero entero entre 0 y 255")
+ 		ElementosIP2 = []
+ 	}else{
+ 		return ("OK")
+ 		segundaIP(ElementosIP2)
+ 		ElementosIP2 = []
+ 	}
+ }
